c5c62b86ecb0a9fee11506ad48d199c14a814a8b
HankChan11 <HankChan11@gmail.com>
2025-09-02 15:45:17 +0800

    build: deploy to /docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,4 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './styles.css'
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+﻿import React from "react";
+import { createRoot } from "react-dom/client";
+import "./main.css";
+createRoot(document.getElementById("root")).render(<h1>2026 春酒抽獎</h1>);
